75c3bef5e5f6fbe5ea865d0d11140076b3d56399
Robbie Boughton <ue20841@bristol.ac.uk>
2025-11-20 12:57:41 +0000

    Decided to create global ML model first. Created 4 week lag and 4 week rolling average - store and dept specific

--- main.py
+++ main.py
@@ -19,16 +19,34 @@ train = pd.read_csv('train.csv.zip')
 merged_train = train.merge(features, on=["Store","Date"], how="left")
 
 for c in merged_train.columns:
-    if merged_train[c].isna().any():
-        print(c)
+    # if merged_train[c].isna().any():
+    #     print(c)
     if c.startswith("MarkDown"):
         merged_train[c].fillna(0, inplace=True)
 
 # Sorting Values before lagging values to ensure correct chronologics.
-merged_train.sort_values(['Store','Dept','Date'])
+merged_train = merged_train.sort_values(['Store','Dept','Date']).reset_index(drop=True)
 
 # Ensuring date values are datetime and not string
 merged_train['Date'] = pd.to_datetime(merged_train['Date'])
 
 # Lagging process
 # merged_train['lag_1'] = merged_train.groupby(['Store','Date'])['Weekly_Sales'].shift(1)
+
+# Adding 4 week lag process (for 4 week rolling average)
+for k in [1,2,3,4]:
+    merged_train[f"lag_{k}"] = (
+        merged_train
+        .groupby(["Store","Dept"])["Weekly_Sales"]
+        .shift(k)
+    )
+    
+# Implementing rolling average
+merged_train["roll_mean_4"] = (
+    merged_train
+    .groupby(["Store","Dept"])["Weekly_Sales"]
+    .apply(lambda s: s.shift(1).rolling(window=4, min_periods=1).mean())
+)
+
+# Creating new merged df without empty lag_1s
+merged_model = merged_train.dropna(subset=["lag_1"]).copy()
